04e4d175f51453fbec289cfaaf6b77c0f75b3a4b
D3Turquoise <anastasija_toleva@yahoo.com>
2023-02-12 14:56:00 +0000

    working on JS file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TODO: Style current HTML
 
-const apiKey = PUT YOUR API KEY HERE!;
+const apiKey = (`576fa128ee8a36649e806e305a625c09`);
 const history = JSON.parse(localStorage.getItem('history')) || [];
 // TODO: Populate history list from local storage when page loads
 
@@ -26,13 +26,49 @@ $('#search-form').on('submit', function(event) {
             // Call 5 day weather forecast API after we have city lat and lon value
             $.ajax({ url: weatherQueryUrl })
                 .then(function(weatherResponse) {
+                    console.log(weatherResponse)
                     // Put the response on the HTML page
                     const weatherList = weatherResponse.list;
 
                     const weathers = [];
                     for (let i = 0; i < weatherList.length; i += 8) {
+                        // i = 0 + 8 ---> i = 8
+                        // 
                         weathers.push(weatherList[i]);
                     }
+
+                    console.log(weathers)
+
+                    // <div class="card">
+                        // <div class="card-header">
+                        //   <h4>Date</h4>
+                        //   <img class ="weather-icon" src="https://via.placeholder.com/150"/>
+                        // </div>
+                        // <ul class="list-group list-group-flush">
+                        //      <li class="list-group-item">Temp: </li>
+                        //      <li class="list-group-item">Wind: </li>
+                        //      <li class="list-group-item">Humidity: </li>
+                        // </ul>
+                    // </div>
+
+                    for(i = 0; i < weathers.length; i++) {
+                        const cardDiv = $("<div class='card'>")
+                        cardDiv.html(`
+                                    <div class="card-header">
+                                    <h4>${moment(weathers[i].dt*1000).format("DD/MM/YYYY")}</h4>
+                                    <img class ="weather-icon" src=" http://openweathermap.org/img/wn/${weathers[i].weather[0].icon}@2x.png"/>
+                                </div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Temp: ${weathers[i].main.temp}</li>
+                                    <li class="list-group-item">Wind: ${weathers[i].wind.speed}</li>
+                                    <li class="list-group-item">Humidity: ${weathers[i].main.humidity}</li>
+                                </ul>
+                        `)
+
+                        $("#forecast").append(cardDiv)
+                    }
+
+
 
                     // weathers[0] will be today's weather
                     // weathers[1 - 4] will be 5 days forecast
